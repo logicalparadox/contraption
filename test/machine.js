c2a908bb83bf5f4d709bdf556169e9f8cc57581a
Jake Luer <jake@alogicalparadox.com>
2013-09-22 02:53:29 -0400

    test: machines

--- test/machine.js
+++ test/machine.js
@@ -1,6 +1,27 @@
-describe('Machine', function () {
-  describe('(construction)', function () {
-    it('should throw on unexpected deadlock', function () {
+var EventEmitter = require('drip').EventEmitter;
+
+function cycle() {
+  var builder = contraption();
+
+  builder.method('start', {
+      from: [ 'none', 'stopped' ]
+    , next: 'started'
+    , handle: function () {}
+  });
+
+  builder.method('stop', {
+      from: 'started'
+    , during: 'stopping'
+    , next: 'stopped'
+    , handle: function (ev, done) {}
+  });
+
+  return builder;
+}
+
+describe('Contraption', function () {
+  describe('.build()', function () {
+    it('throws on unexpected deadlock', function () {
       var builder = contraption();
 
       builder.method('start', {
@@ -21,28 +42,14 @@ describe('Machine', function () {
       }).should.throw(/Unexpected deadlock state\(s\): stopped/)
     });
 
-    it('should not throw if no deadlock exists', function () {
-      var builder = contraption();
-
-      builder.method('start', {
-          from: [ 'none', 'stopped' ]
-        , next: 'started'
-        , handle: function () {}
-      });
-
-      builder.method('stop', {
-          from: 'started'
-        , during: 'stopping'
-        , next: 'stopped'
-        , handle: function (ev, done) {}
-      });
-
+    it('does not throw if no deadlock exists', function () {
+      var builder = cycle();
       (function () {
         builder.build();
       }).should.not.throw();
     });
 
-    it('should not throw if deadlock defined', function () {
+    it('does not throw if deadlock defined', function () {
       var builder = contraption();
 
       builder.method('start', {
@@ -63,6 +70,62 @@ describe('Machine', function () {
       (function () {
         builder.build();
       }).should.not.throw();
+    });
+  });
+
+  describe('.call(obj, key)', function() {
+    it('mounts machine to key on target', function() {
+      var _ = {};
+      contraption().call(_);
+      _.should.have.property('_fsm');
+    });
+
+    it('binds machine events to target', function() {
+      var _ = EventEmitter({});
+      var started = chai.spy('started');
+      cycle().call(_);
+      _.on('started', started);
+      _._fsm.emit('started');
+      started.should.have.been.called(1);
+    });
+  });
+
+  describe('.mixin(obj, key)', function() {
+    it('mounts all methods', function() {
+      var _ = {};
+      var builder = cycle();
+      builder.call(_);
+      builder.mixin(_);
+      _.should.itself.respondTo('start');
+      _.should.itself.respondTo('stop');
+    });
+
+    it('throws error if method already defined', function() {
+      var _ = { start: function() {} };
+      var builder = cycle();
+      builder.call(_);
+      (function() {
+        builder.mixin(_);
+      }).should.throw(/Method \"start\" already defined\./);
+    });
+
+    describe('mounted methods', function() {
+      it('calls .invoke() of the mounted machine', function() {
+        var _ = {};
+        var builder = cycle();
+        builder.call(_);
+        builder.mixin(_);
+
+        var invoke = chai.spy('invoke', function(method, args, ctx) {
+          method.should.equal('start');
+          args.should.deep.equal([ 1, 2, 3 ]);
+        });
+
+        _._fsm.invoke = invoke;
+
+        _.start(1, 2, 3);
+        invoke.should.have.been.called(1);
+      });
     });
   });
 });
